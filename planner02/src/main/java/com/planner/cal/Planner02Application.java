f88fd49673cc53ab7228cd421bf3fea855efdcc6
choiseokyong <ggt9875654@naver.com>
2025-05-01 16:41:29 +0900

    첫 푸시 - 일정 등록 작업 중

---
+++ planner02/src/main/java/com/planner/cal/Planner02Application.java
@@ -0,0 +1,13 @@
+package com.planner.cal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Planner02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Planner02Application.class, args);
+	}
+
+}
---
+++ planner02/src/test/java/com/planner/cal/Planner02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planner.cal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Planner02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
